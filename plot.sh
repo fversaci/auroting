207a23ce93c2da79d11fbeaa8d1f07d28edb1188
Francesco Versaci <francesco.versaci@gmail.com>
2013-06-18 16:58:18 +0200

    Launched 16x16x16 simulations

--- plot.sh
+++ plot.sh
@@ -2,25 +2,28 @@
 
 tmp=$(tempfile) || exit
 
-for PATT in Bitreverse Butterfly Uniform Transposition TDTrans; do
+for PATT in Bitreverse Butterfly Uniform Transposition TDTrans Bitcomplement; do
     last=`ls $PATT-* | cut -d "-" -f 2 | cut -d "." -f 1 | sort -n | tail -n 1`
     echo $PATT: $last
-    step=3
+    step=4
     for alg in `seq 0 $((step-1))`; do
-	echo "$PATT Alg $alg, lifetime:" >> "$tmp";
-	for n in `seq $alg $step $last`; do
-	    #echo -n "$n: " >> "$tmp";
-	    grep totalDerouted $PATT-$n.sca | cut -c 43- | tr -d '\n' >> "$tmp";
-	    echo -n ", " >> "$tmp";
-	done
-	echo "\n" >> "$tmp";
-	#echo "$PATT Alg $alg:, hoptime" >> "$tmp";
+	#echo "$PATT Alg $alg, derouted:" >> "$tmp";
 	#for n in `seq $alg $step $last`; do
 	#    #echo -n "$n: " >> "$tmp";
-	#    grep mean $PATT-$n.sca | head -n 1 | cut -c 12- | tr -d '\n' >> "$tmp";
+	#    grep totalDerouted $PATT-$n.sca | cut -c 43- | tr -d '\n' >> "$tmp";
 	#    echo -n ", " >> "$tmp";
 	#done
 	#echo "\n" >> "$tmp";
+	##########################
+	##########################
+	echo "$PATT Alg $alg:, lifetime" >> "$tmp";
+	for n in `seq $alg $step $last`; do
+	    #echo -n "$n: " >> "$tmp";
+	    #grep mean $PATT-$n.sca | head -n 1 | cut -c 12- | tr -d '\n' >> "$tmp";
+	    grep mean $PATT-$n.sca | tail -n 1 | cut -c 12- | tr -d '\n' >> "$tmp";
+	    echo -n ", " >> "$tmp";
+	done
+	echo "\n" >> "$tmp";
     done
 done
 
